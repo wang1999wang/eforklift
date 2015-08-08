1ca45e159943eee22f3d6c3241203cff14c73e9d
wang1999wang <wang1999wang@foxmail.com>
2015-08-09 07:17:42 +0800

    123
    Signed-off-by: wang1999wang <wang1999wang@foxmail.com>

---
+++ app/controller/about/index.class.php
@@ -0,0 +1,23 @@
+<?php
+/*
+* $Author ：PHPYUN开发团队
+*
+* 官网: http://www.phpyun.com
+*
+* 版权所有 2009-2015 宿迁鑫潮信息技术有限公司，并保留所有权利。
+*
+* 软件声明：未经授权前提下，不得用于商业运营、二次开发以及任何形式的再次发布。
+ */
+class index_controller extends common{ 
+	function index_action(){
+        $adlist=$this->MODEL()->DB_select_all('ad');
+        foreach($adlist as $k1=>$v1){
+            $adlist_new[$v1['id']]=array('pic'=>FormatPicUrl(array('path'=>$v1['pic_url'])));
+        }
+        $this->yunset('position_list',array(970,1660,2420,3215,3960,4630,5490,6350,7260));
+        $this->yunset('adlist',$adlist_new);
+		$this->seo("echapinpai");
+		$this->yun_tpl(array('index'));
+	} 
+}
+?>
---
+++ config/db.config.php
@@ -0,0 +1,15 @@
+<?php 
+  $db_config = array(
+      'dbtype'=>'mysql',
+      'dbhost'=>'211.149.239.16',
+      'dbuser'=>'echatest',
+      'dbpass'=>'echatest01',
+      'dbname'=>'echatest',
+      'def'=>'phpyun_',
+      'charset'=>'GBK',
+      'timezone'=>'PRC',
+      'coding'=>'5607343f66f33ccec9c7f0bf33a388fa', //生成cookie加密
+      'version'=>'4.0 Beta',//版本号
+    );
+    
+?>
