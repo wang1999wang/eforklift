c785695ec2248fec33e4b7371ca676f89614dbca
wang1999wang <wang1999wang@foxmail.com>
2015-09-01 08:26:09 +0800

    Signed-off-by: wang1999wang <wang1999wang@foxmail.com>

--- app/controller/search/index.class.php
+++ app/controller/search/index.class.php
@@ -8,7 +8,14 @@ class index_controller extends common{
         global $CONFIG;
         $q=trim($_GET['q']);
         $M=$this->MODEL('article');
-        $datas=$M->DB_select_all('news_base',"title like '%{$q}%' or content like '%{$q}%' or description like '%{$q}%'");
+        $sql="";
+        $datas=$M->DB_query_all("select * from (
+SELECT id,title,'cars' as con,lastupdate FROM `phpyun_cars_base` WHERE title like '%{$q}%' or content like '%{$q}%' or description like '%{$q}%'
+union
+SELECT id,title,'article' as con,lastupdate FROM `phpyun_news_base` WHERE title like '%{$q}%' or content like '%{$q}%' or description like '%{$q}%'
+union
+SELECT id,title,'school' as con,lastupdate FROM `phpyun_school_base` WHERE title like '%{$q}%' or content like '%{$q}%' or description like '%{$q}%'
+) a order by a.lastupdate desc ","all");//print_r($datas);
         $this->yunset('datas',$datas);
         $this->yunset('position_list',array(970,1660,2420,3215,3960,4630,5490,6350,7260));
         $this->yunset('adlist',$adlist_new);
